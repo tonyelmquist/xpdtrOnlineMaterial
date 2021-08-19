56a9fc218d326d9d6777c6af0615d230be235e11
Tony Elmquist <tony@klavenessdigital.com>
2021-08-19 16:31:07 +0200

    working with save agail

--- src/pages/filings/PSPDFKit.js
+++ src/pages/filings/PSPDFKit.js
@@ -4,11 +4,8 @@ import { Save } from "@material-ui/icons";
 const PSPDFKIT = (props) => {
   const containerRef = useRef(null);
 
-  const saveTheThings = () => {
-    this._instance.exportInstantJSON().then((instantJSON) => {
-      const theContent = JSON.stringify(instantJSON);
-      this.props.saveFiling(theContent, this.props.formId);
-    });
+  const saveTheThings = (content) => {
+    props.saveFiling(content, props.formId);
   };
 
   console.log(Save);
@@ -18,37 +15,24 @@ const PSPDFKIT = (props) => {
     let instance, PSPDFKit;
     (async function () {
       PSPDFKit = await import("pspdfkit");
-        const downloadButton = {
-          type: "custom",
-          id: "download-pdf",
-          icon: Save,
-          title: "Download",
-          onPress: () => {
-            this._instance.exportPDF().then((buffer) => {
-              const blob = new Blob([buffer], { type: "application/pdf" });
-              const fileName = "document.pdf";
-              if (window.navigator.msSaveOrOpenBlob) {
-                window.navigator.msSaveOrOpenBlob(blob, fileName);
-              } else {
-                const objectUrl = window.URL.createObjectURL(blob);
-                const a = document.createElement("a");
-                a.href = objectUrl;
-                a.style = "display: none";
-                a.download = fileName;
-                document.body.appendChild(a);
-                a.click();
-                window.URL.revokeObjectURL(objectUrl);
-                document.body.removeChild(a);
-              }
-            });
-          },
-        };
+      const downloadButton = {
+        type: "custom",
+        id: "save",
+        icon: "/disk.svg",
+        title: "Download",
+        onPress: () => {
+          instance.exportInstantJSON().then((instantJSON) => {
+            const theContent = JSON.stringify(instantJSON);
+            saveTheThings(theContent);
+          });
+        },
+      };
 
-        const items = PSPDFKit.defaultToolbarItems;
-        // Add the download button to the toolbar.
-        items.push(downloadButton);
+      const items = PSPDFKit.defaultToolbarItems;
+      // Add the download button to the toolbar.
+      items.push(downloadButton);
 
-        console.log(items);
+      console.log(items);
 
       if (props.content) {
         instance = await PSPDFKit.load({
@@ -58,7 +42,14 @@ const PSPDFKIT = (props) => {
           document: props.pdfUrl,
           printMode: PSPDFKit.PrintMode.EXPORT_PDF,
           toolbarItems: items,
-          //  instantJSON: JSON.parse(props.content),
+          instantJSON: JSON.parse(props.content),
+          electronicSignatures: {
+            creationModes: [
+              PSPDFKit.ElectronicSignatureCreationMode.IMAGE,
+              PSPDFKit.ElectronicSignatureCreationMode.DRAW,
+              PSPDFKit.ElectronicSignatureCreationMode.TYPE,
+            ],
+          },
           // Use the public directory URL as a base URL. PSPDFKit will download its library assets from here.
           baseUrl: `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}`,
         });
@@ -70,13 +61,18 @@ const PSPDFKIT = (props) => {
           document: props.pdfUrl,
           printMode: PSPDFKit.PrintMode.EXPORT_PDF,
           toolbarItems: items,
+          electronicSignatures: {
+            creationModes: [
+              PSPDFKit.ElectronicSignatureCreationMode.IMAGE,
+              PSPDFKit.ElectronicSignatureCreationMode.DRAW,
+              PSPDFKit.ElectronicSignatureCreationMode.TYPE,
+            ],
+          },
 
           // Use the public directory URL as a base URL. PSPDFKit will download its library assets from here.
           baseUrl: `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}`,
         });
       }
-
-    
     })();
 
     return () => PSPDFKit && PSPDFKit.unload(container);
